86218503a8e53399fcaebc7daabd9d16705769f3
Richard Bernardino <rmbernardi@gmail.com>
2021-03-05 10:02:06 -0800

    Update RandomExample.java
    Adding more detailed description of Math.random()

--- RandomExample.java
+++ RandomExample.java
@@ -4,7 +4,12 @@ class RandomExample
 {
 	public static void main(String args[])
 	{
+		// Math.random() return a floating-point
+		// number in the range of 0.0 to 1.0.
 		double random = Math.random();
+		
+		// Normalize the random double to a number
+		// within 1 to 10.
 		int normalized = (int)(random * 10.0) + 1;
 
 		System.out.println(
