0a1d858ef268aa12fe32d733e73d0962cb754a9a
Richard Bernardino <rmbernardi@gmail.com>
2021-03-11 18:41:18 -0800

    Create CyYoung.java

---
+++ CyYoung.java
@@ -0,0 +1,39 @@
+/*****************************************************************************
+Copyright (c) 2021 Orchidware Studios LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*****************************************************************************/
+import java.io.*;
+
+public class CyYoung extends Pitcher 
+{
+    public static void main(String args[]) 
+    {
+        Pitcher p = new CyYoung();
+
+	p.pitch();
+    }
+
+    protected void windup() { System.out.println("Cy's Windup"); }
+    protected void stride() { System.out.println("Cy's Stride"); }
+    protected void phase3() { System.out.println("Cy's Phase 3"); }
+    protected void armAcceleration() { System.out.println("Cy's Arm Acceleration"); }
+    protected void armDeceleration() { System.out.println("Cy's Arm Deceleration"); }
+    protected void followThrough() { System.out.println("Cy's Follow Through"); }
+}
