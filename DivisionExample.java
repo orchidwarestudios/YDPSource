e4d2d45301741021ed409ec1e9b0c0e2efc1ef88
Richard Bernardino <rmbernardi@gmail.com>
2021-03-12 03:14:54 -0800

    Adding website header

--- DivisionExample.java
+++ DivisionExample.java
@@ -19,6 +19,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *****************************************************************************/
+
+/*****************************************************************************
+ For more information, visit us at http://orchidwarestudios.com 
+*****************************************************************************/
+
 import java.io.*;
 
 class DivisionExample
