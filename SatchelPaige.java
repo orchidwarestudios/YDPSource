26ce8ab032b78c5f248ff112702df36d3fa20670
Richard Bernardino <rmbernardi@gmail.com>
2021-03-16 06:49:31 -0700

    Create SatchelPaige.java

---
+++ SatchelPaige.java
@@ -0,0 +1,44 @@
+/*****************************************************************************
+Copyright (c) 2021 Orchidware Studios LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*****************************************************************************/
+
+/*****************************************************************************
+ For more information, visit us at http://orchidwarestudios.com 
+*****************************************************************************/
+
+import java.io.*;
+
+public class SatchelPaige extends Pitcher 
+{
+    public static void main(String args[]) 
+    {
+        Pitcher p = new SatchelPaige();
+
+	p.pitch();
+    }
+
+    protected void windup() { System.out.println("Satchel's Windup"); }
+    protected void stride() { System.out.println("Satchel's Stride"); }
+    protected void phase3() { System.out.println("Satchel's Phase 3"); }
+    protected void armAcceleration() { System.out.println("Satchel's Arm Acceleration"); }
+    protected void armDeceleration() { System.out.println("Satchel's Arm Deceleration"); }
+    protected void followThrough() { System.out.println("Satchel's Follow Through"); }
+}
